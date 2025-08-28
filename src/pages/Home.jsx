6a72c6d72c98ace3ee107b3d0347f771a1946169
unknown <alexandre.hariri@orange.fr>
2025-08-28 11:56:32 +0200

    correction des chemin des assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
       <section className="features">
         <h2 className="sr-only">Features</h2>
         <div className="feature-item">
-          <img src="assets/icon-chat.png" alt="Chat Icon" className="feature-icon" />
+          <img src="/Argent-Bank-FrontEnd/assets/icon-chat.png" alt="Chat Icon" className="feature-icon" />
           <h3 className="feature-item-title">You are our #1 priority</h3>
           <p>
             Need to talk to a representative? You can get in touch through our
@@ -22,7 +22,7 @@ function Home() {
         </div>
         <div className="feature-item">
           <img
-            src="assets/icon-money.png"
+            src="/Argent-Bank-FrontEnd/assets/icon-money.png"
             alt="Chat Icon"
             className="feature-icon"
           />
@@ -33,7 +33,7 @@ function Home() {
         </div>
         <div className="feature-item">
           <img
-            src="assets/icon-security.png"
+            src="/Argent-Bank-FrontEnd/assets/icon-security.png"
             alt="Chat Icon"
             className="feature-icon"
           />
